b9998efce3ad4b9cd145ae57e1a70dd6fab16714
chiraagbalajip30 <chiraagbalajip30@gmail.com>
2025-12-31 16:47:41 +0530

    basic bug fix

--- dynamic-info-banner/vite.config.js
+++ dynamic-info-banner/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Week-4-Day-4-Task/",
+  base: '/Week-4-Day-4-Task/',
 });
